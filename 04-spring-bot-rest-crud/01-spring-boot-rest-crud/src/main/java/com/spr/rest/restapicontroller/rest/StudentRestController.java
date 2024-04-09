0bb0d30d6dfbd160ce6cf3ae93a4a62b9b43e170
Prasanna Srinivasan <github@prasansrini.com>
2024-04-09 16:32:53 +0200

    Fourth chapter ExceptionHandler for Rest with @ControllerAdvice.

--- 04-spring-bot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/spr/rest/restapicontroller/rest/StudentRestController.java
+++ 04-spring-bot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/spr/rest/restapicontroller/rest/StudentRestController.java
@@ -1,9 +1,12 @@
 package com.spr.rest.restapicontroller.rest;
 
 import com.spr.rest.restapicontroller.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spr.rest.restapicontroller.exception.StudentErrorResponse;
+import com.spr.rest.restapicontroller.exception.StudentNotFoundException;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +14,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    // Define an endpoint for "/students"
+    private List<Student> theStudents;
 
+    @PostConstruct
+    public void loadStudent() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Prasanna", "Srinivasan"));
+        theStudents.add(new Student("Sundari", "Srinivasan"));
+        theStudents.add(new Student("Rajakamalam", "Mariappan"));
+    }
+
+    // Define an endpoint for "/students"
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+        return theStudents;
+    }
 
-        students.add(new Student("Prasanna", "Srinivasan"));
-        students.add(new Student("Sundari", "Srinivasan"));
-        students.add(new Student("Rajakamalam", "Mariappan"));
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        if (studentId >= theStudents.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student [" + studentId + "] not found!");
+        }
 
-        return students;
+        return theStudents.get(studentId);
     }
 }
