8cbeaac0799aa4330a60a7e67ca039217e127309
Prasanna <github@prasansrini.com>
2024-04-15 14:29:18 +0200

    Fifth chapter Spring boot security with users, passwords and roles. Added Basic authentication.

--- 04-spring-bot-rest-crud/05-spring-boot-rtest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/spr/springboot/security/demo/security/DemoSecurityConfig.java
+++ 04-spring-bot-rest-crud/05-spring-boot-rtest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/spr/springboot/security/demo/security/DemoSecurityConfig.java
@@ -2,9 +2,14 @@ package com.spr.springboot.security.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -33,4 +38,20 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, prasan, mons);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 }
