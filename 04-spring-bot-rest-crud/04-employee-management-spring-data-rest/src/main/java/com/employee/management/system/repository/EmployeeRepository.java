fff58154cd9f7c78a1c0edc88050fd3669ce5c87
Prasanna <github@prasansrini.com>
2024-04-15 10:06:26 +0200

    Fourth chapter Employee Management System with Spring Data Rest. Without coding. :|

---
+++ 04-spring-bot-rest-crud/04-employee-management-spring-data-rest/src/main/java/com/employee/management/system/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.employee.management.system.repository;
+
+import com.employee.management.system.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
