f204cd881c33fe21e7a0a3f7a67b96ba34287969
Prasanna <github@prasansrini.com>
2024-04-15 10:41:09 +0200

    Fourth chapter Employee Management System with Spring Data Rest with @RepositoryRestResource for custom path for the api.

--- 04-spring-bot-rest-crud/04-employee-management-spring-data-rest/src/main/java/com/employee/management/system/repository/EmployeeRepository.java
+++ 04-spring-bot-rest-crud/04-employee-management-spring-data-rest/src/main/java/com/employee/management/system/repository/EmployeeRepository.java
@@ -2,9 +2,9 @@ package com.employee.management.system.repository;
 
 import com.employee.management.system.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
