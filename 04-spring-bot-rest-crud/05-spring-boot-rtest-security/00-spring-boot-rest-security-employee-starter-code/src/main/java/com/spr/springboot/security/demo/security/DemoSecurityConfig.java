4f2e26e1155fcf096e15f1ab3091b9cfda0120da
Prasanna <github@prasansrini.com>
2024-04-15 15:10:18 +0200

    Fifth chapter Spring boot security with JDBCUserDetailsManager. Added Basic authentication.

--- 04-spring-bot-rest-crud/05-spring-boot-rtest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/spr/springboot/security/demo/security/DemoSecurityConfig.java
+++ 04-spring-bot-rest-crud/05-spring-boot-rtest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/spr/springboot/security/demo/security/DemoSecurityConfig.java
@@ -6,36 +6,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails prasan = User.builder()
-                .username("prasan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails mons = User.builder()
-                .username("mons")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, prasan, mons);
+    public UserDetailsManager filterChainFromDb(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
