90cf5ea86a6350dc8d32e1d69f1fa99c959bdf6c
Prasanna <github@prasansrini.com>
2024-04-17 15:35:15 +0200

    Chapter six Thymeleaf templating engine by manupulating the request param before responding.

--- 04-spring-bot-rest-crud/06-spring-mvc/01-spring-thymeleaf/src/main/java/com/spr/thymeleaf/ThymeLeafSample/controller/HelloWorldController.java
+++ 04-spring-bot-rest-crud/06-spring-mvc/01-spring-thymeleaf/src/main/java/com/spr/thymeleaf/ThymeLeafSample/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.spr.thymeleaf.ThymeLeafSample.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +16,22 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processAndRespondForm() {
         return "processed-form";
+    }
+
+    @RequestMapping("/processFormAgain")
+    public String processInformation(HttpServletRequest request, Model model) {
+        // Read request param from HTML form.
+        String name = request.getParameter("studentName");
+
+        // Convert the data to upper case.
+        name = name.toUpperCase();
+
+        // Create a message.
+        String result = "Hello " + name + "!";
+
+        // Add the message to the model.
+        model.addAttribute("message", result);
+
+        return "helloworld-new";
     }
 }
