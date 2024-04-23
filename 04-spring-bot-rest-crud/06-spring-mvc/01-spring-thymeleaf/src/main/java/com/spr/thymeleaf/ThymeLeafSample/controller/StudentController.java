69661946614e33b1f0daffd3255077dcd5d1ffdd
Prasanna <github@prasansrini.com>
2024-04-23 15:23:20 +0200

    Chapter six Dynamic content from application.properties.

--- 04-spring-bot-rest-crud/06-spring-mvc/01-spring-thymeleaf/src/main/java/com/spr/thymeleaf/ThymeLeafSample/controller/StudentController.java
+++ 04-spring-bot-rest-crud/06-spring-mvc/01-spring-thymeleaf/src/main/java/com/spr/thymeleaf/ThymeLeafSample/controller/StudentController.java
@@ -1,25 +1,31 @@
 package com.spr.thymeleaf.ThymeLeafSample.controller;
 
 import com.spr.thymeleaf.ThymeLeafSample.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countryList;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countryList);
 
         return "student-form";
     }
 
     @PostMapping("/processStudentForm")
     public String processStudentForm(@ModelAttribute("student") Student student) {
-
         System.out.println(student);
 
         return "student-confirmation";
