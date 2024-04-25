58d4fb504870813f384a07163a49e2faaf621a84
Prasanna <github@prasansrini.com>
2024-04-25 11:35:09 +0200

    Chapter six String trimmer in form validation

--- 04-spring-bot-rest-crud/06-spring-mvc/02-springthymeleaf-form-validation/src/main/kotlin/com/form/validation/formvalidation/controller/CustomerController.kt
+++ 04-spring-bot-rest-crud/06-spring-mvc/02-springthymeleaf-form-validation/src/main/kotlin/com/form/validation/formvalidation/controller/CustomerController.kt
@@ -2,22 +2,29 @@ package com.form.validation.formvalidation.controller
 
 import com.form.validation.formvalidation.model.Customer
 import jakarta.validation.Valid
+import org.springframework.beans.propertyeditors.StringTrimmerEditor
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
+import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.InitBinder
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class CustomerController {
 
+	@InitBinder
+	public fun initBinder(dataBinder: WebDataBinder) {
+		val strippedString = StringTrimmerEditor(true)
+
+		dataBinder.registerCustomEditor(String::class.java, strippedString)
+	}
+
 	@GetMapping("/")
 	public fun showForm(model: Model): String {
-		model.addAttribute("customer", Customer().apply {
-			firstName = ""
-			lastName = ""
-		})
+		model.addAttribute("customer", Customer())
 
 		return "customer-form"
 	}
@@ -26,6 +33,7 @@ class CustomerController {
 	public fun processForm(
 		@Valid @ModelAttribute("customer") customer: Customer, bindingResult: BindingResult
 	): String {
+		println("Last name: [${customer.lastName}]")
 		return if (bindingResult.hasErrors()) "customer-form" else "customer-confirmation"
 	}
 }
